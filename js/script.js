c0e1f191b772d1ef2e10a1a6951bbf6eabac9907
Charlotte Gisele Weil <weilcharlotte@gmail.com>
2017-03-30 10:45:49 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+function PlaySound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    //thissound.currentTime = 0;   //Add this line if I want to re-start sound from beginning on hover
+    thissound.play();
+}
+
+function StopSound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.pause();
+}
+
+
+function generateLaughTemplate(d) {
+  var template = $('#template').html();
+  
+  var rendered = Mustache.render(template, {name: d});
+  return rendered;
+}
+
+
+
+//Dataset
+     var laughs = ['ami','jack','rupa','elliot','angie','annalee',
+        'damien','laura','kamakshi',
+        'charlie','tareq','daniel','isaac','ian','caroline',
+        'karen','jan','john','catherine','karissa',
+        'kelley','lee','lena','maria','wilson','omar',
+        'raquel','rob','tasha','tinker','tucker'];
+
+
+
+
+$(document).ready(main);
+
+function main() {
+
+    var baseDiv = d3.select('#container_div');
+       console.log('treter', laughs, baseDiv);
+
+    baseDiv.selectAll('.laugh')
+        .data(laughs)
+        .enter()
+        .append('div.laugh').html(generateLaughTemplate)
+}
+
+
+        
